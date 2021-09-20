7f0e5aa6ce98a9f13a0a3c373fe17d509eba87c2
PauloRubim <paulo789rubim@gmail.com>
2021-09-19 21:09:14 -0300

    Mine projetos em python

---
+++ madlib/madlibs.py
@@ -0,0 +1,15 @@
+# youtuber = "Paulo"
+
+# print('subscribe to ' + youtuber)
+# print('subscribe to {}'.format(youtuber))
+# print(f'subscribe to {youtuber}')
+
+adj = input("Adjective: ")
+verb1 = input("Verb one: ")
+verb2 = input("Verb two: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programming is so {adj}!, It makes me so exited all the time becase\
+    I love to {verb1}, stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
---
+++ projeto3/paper_rock_scissors.py
@@ -0,0 +1,21 @@
+from random import choice
+
+def play():
+    user = input("What's your choice? 'r', for rock, 'p' for paper, 's' for scissors\n ")
+    computer = choice(['r', 'p', 's'])
+
+    if user == computer: 
+        return 'It\'s a tie'
+    
+    if is_win(user, computer):
+        return 'You won!'
+
+    return 'You lost!'
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 'r') or (player == 's' and opponent == 'p')\
+        or (player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
+
